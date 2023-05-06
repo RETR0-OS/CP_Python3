fce1b27edecbe2f849e146d14bb530b2388cd52e
RETR0-OS <74290459+RETR0-OS@users.noreply.github.com>
2023-05-06 15:49:49 +0530

    Create SumOfNNumbers.py

---
+++ NumberTheory/SumOfNNumbers.py
@@ -0,0 +1,6 @@
+# The sun of N natural numbers can always be found using the formula of sum of n terms of an AP 
+#sum = ((n)*(n-1))//2
+#This formula gies the sum of n natural numbers in the most efficient way
+
+n = int(input("Enter maximum number>> "))
+return ((n)*(n-1))//2
