78022d8614a22a20c24ea941cc226abab9e3ce4f
RETR0-OS <74290459+RETR0-OS@users.noreply.github.com>
2023-05-06 16:04:02 +0530

    Update FindFactors.py

--- NumberTheory/FindFactors.py
+++ NumberTheory/FindFactors.py
@@ -2,7 +2,7 @@
 from math import sqrt
 def get_divisors(n):
   div = set()
-  for i in range(1, sqrt(n)+1):
+  for i in range(1, int(sqrt(n)+1)):
     if n%i == 0:
       div.add(i)
       div.add(n//i)
