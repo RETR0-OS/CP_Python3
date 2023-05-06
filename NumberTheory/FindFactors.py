1cdf3fdb22164a1b397206108851d5b8766acbb3
RETR0-OS <74290459+RETR0-OS@users.noreply.github.com>
2023-05-06 16:02:10 +0530

    Create FindFactors.py

---
+++ NumberTheory/FindFactors.py
@@ -0,0 +1,12 @@
+#This algorithm returns all possible factors of a number in in O(sqrt(n)) time complexity.
+from math import sqrt
+def get_divisors(n):
+  div = set()
+  for i in range(1, sqrt(n)+1):
+    if n%i == 0:
+      div.add(i)
+      div.add(n//i)
+  return list(div)
+
+in_ = int(input("Enter n>>" ))
+print(get_divisors(in_))
